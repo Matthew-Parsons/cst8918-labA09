e32b5ac80299655ee4595ed7c32eac998e18f1b8
Matthew-Parsons <pars0230@algonquinlive.com>
2026-03-19 18:27:07 -0400

    testing

--- main.tf
+++ main.tf
@@ -27,14 +27,14 @@ provider "cloudinit" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.labelPrefix}-A09-RG"
-  location = var.
+  location = var.region
 }
 
 
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  name                = var.storage_account_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
